f29e98043e95d1923f8d2298f09e215ef5bd5764
Emil Mariya Benny <emilmariyabenny2001@gmail.com>
2022-02-06 19:02:16 +0530

    Update disk_space_management.sh

--- disk_space_management.sh
+++ disk_space_management.sh
@@ -6,16 +6,16 @@ th_value=50
 actual_value_space=`df -h  /dev/xvda1  | awk 'FNR==2{print $5}'  |tr -d '%'`
 
 
-echo "$date | INFO | Getted current value of disk space is $actul_value_space%"
-echo "$date | INFO | our th value is $th_value%"
+echo "$date | INFO | Got current value of disk space as $actul_value_space%"
+echo "$date | INFO | our th_value is $th_value%"
 
 #main  loop
 if (( $actual_value_space >= $th_value ))
 then
-	 echo "$date | INFO | current value $actul_value_space% is grater than th value"
+	 echo "$date | INFO | current value $actul_value_space% is greater than th_value"
          echo "$date | INFO | hence movd the path $path"
 
 	cd $path
         >Elektron_BL.log
-	echo "$date | INFO | file cleraed successfully on thhe path $path"
+	echo "$date | INFO | file cleared successfully on the path $path"
 fi
